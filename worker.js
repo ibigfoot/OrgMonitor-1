78ceb1380e3b116e763154405e83316634527aef
Troy Sellers <tsellers@salesforce.com>
2018-02-03 15:15:20 +1100

    fix

--- worker.js
+++ worker.js
@@ -9,7 +9,7 @@ const queue = require('./lib/kue.js')
 
 queue.process('refreshOrg', function(job, done) {
   const jobData = job.data
-  console.log(`[${jobData.orgId}] Syncing..`)
+  
   let org = await Org.get(jobData.orgId)
   let data = await org.fetchRemoteData()
   await Org.saveData(data)
